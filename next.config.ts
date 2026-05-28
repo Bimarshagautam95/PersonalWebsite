3d869c758654bbfc871219b0c5d0234ee7f45553
Bimarsha Gautam <gautambimarsha2003@gmail.com>
2026-05-29 00:03:09 +0545

    redeploy

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,17 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NODE_ENV === "production" ? "/Resume-Nextjs" : "";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath,
-  assetPrefix: basePath,
+  output: "standalone",
+
+
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
+
+  trailingSlash: false,
+
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
   },
 };
 
