2f6a2215bbacac3aaba9ae44b6d1ceccab3f21d7
Bimarsha Gautam <gautambimarsha2003@gmail.com>
2026-05-28 23:39:21 +0545

    New Prototype

---
+++ src/app/components/home/experience-sec/index.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+const ExperienceSec = () => {
+    const experiences = [
+        {
+            year: "2023 - 2025",
+            title: "Business Data and Strategic Analyst",
+            company: "Gayatri Traders",
+            type: "Hybrid",
+            description: ["Analyzed sales, inventory, and operational data to pinpoint trends and operational inefficiencies, driving data-informed business decisions.", 
+            " Designed interactive Power BI and Tableau dashboards to track key metrics and delivered weekly/monthly performance reports for executive forecasting.",  
+            " Collaborated with sales, production, and finance teams to define core KPIs and established validation frameworks to enhance data accuracy."]
+        },
+        {
+            year: "2026",
+            title: "IT Consultant",
+            company: "Sakshyam Gautam & Associates",
+            type: "Part Time",
+            description: ["Led requirements gathering, workflow mapping, and system architecture design for a custom accounting software solution aimed at streamlining business financial operations." ,
+            " Collaborated with development teams and accounting stakeholders to translate complex financial reporting rules, tax tracking, and ledger compliance into scalable technical features.", 
+            " Analyzed user feedback and system workflows to optimize data validation checks, ensuring maximum financial data accuracy and reporting efficiency."]
+        }
+    ];
+
+    return (
+        <section>
+            <div className="py-16 md:py-32">
+                <div className="container mx-auto px-4">
+                    <div className="flex items-center justify-between gap-2 border-b border-black pb-7 mb-9 md:mb-16">
+                        <h2>Experience</h2>
+                        <p className="text-xl text-primary">( 02 )</p>
+                    </div>
+
+                    <div className="space-y-7 md:space-y-12">
+                        {experiences.map((exp, index) => (
+                            <div key={index} className="grid grid-cols-1 sm:grid-cols-3 gap-2.5 md:gap-4 xl:gap-8 items-start relative">
+                                <div className="">
+                                    <h3 className="font-bold mb-2 text-black">{exp.year}</h3>
+                                    <h4 className="text-lg font-normal">{exp.title}</h4>
+                                </div>
+
+                                <div className=" relative">
+                                    {index < experiences.length && (
+                                        <div className={`absolute left-0 top-3 w-px ${index < experiences.length - 1 ? 'h-40' : 'h-30'} bg-softGray`}></div>
+                                    )}
+
+                                    <div className="no-print absolute left-0 top-0 transform -translate-x-1/2">
+                                        <div className={`no-print w-3.5 h-3.5 rounded-full border-1 bg-white flex items-center justify-center ${index === 1 ? 'border-primary' : 'border-black'
+                                            }`}>
+                                            {index === 1 && (
+                                                <div className="w-1.5 h-1.5 rounded-full bg-primary"></div>
+                                            )}
+                                        </div>
+                                    </div>
+
+                                    <div className="pl-4 lg:pl-7">
+                                        <div className="flex items-center gap-2 mb-1">
+                                            <span className="text-xl text-black font-normal">{exp.company}</span>
+                                        </div>
+                                        <p className="text-base font-normal">{exp.type}</p>
+                                    </div>
+                                </div>
+
+                                <div className="pl-8 sm:pl-0">
+                                    <p className="leading-relaxed text-base">{exp.description}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ExperienceSec;
